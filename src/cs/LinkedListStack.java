0f2ac78779209206a756713f4f3164f106fdfdf3
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-14 19:13:03 -0800

    Minor style updates

--- src/cs/LinkedListStack.java
+++ src/cs/LinkedListStack.java
@@ -1,15 +1,13 @@
 package cs;
 
-public class LinkedListStack<T> implements CenturyStackInterface<T>
-{
+public class LinkedListStack<T> implements CenturyStackInterface<T> {
     private Node<T> stackTop;
     private int nodeCount = 0;
 
     /**
      * Constructor sets the first node of the stack to null.
      */
-    public LinkedListStack()
-    {
+    public LinkedListStack() {
         stackTop = null;
     }
 
@@ -17,8 +15,7 @@ public class LinkedListStack<T> implements CenturyStackInterface<T>
      * isEmpty Tests if the stack is empty.
      * @return True if the stack's top node is null, false otherwise.
      */
-    public boolean isEmpty()
-    {
+    public boolean isEmpty() {
         return (stackTop == null);
     }
 
@@ -26,22 +23,18 @@ public class LinkedListStack<T> implements CenturyStackInterface<T>
      * isFull Outputs that the stack is not full.
      * @return False because a stack implemented with a linked list cannot be full.
      */
-    public boolean isFull()
-    {
+    public boolean isFull() {
         return false;
     }
 
     /**
      * pop Throws EmptyStackPop if the stack is empty, otherwise dereferences the most recently pushed node.
      */
-    public void pop()
-    {
+    public void pop() {
         if(isEmpty())
         {
             throw new EmptyStackPop("Pop attempted on an empty stack.");
-        }
-        else
-        {
+        } else {
             stackTop = stackTop.getNext();
         }
         nodeCount--;
@@ -51,8 +44,7 @@ public class LinkedListStack<T> implements CenturyStackInterface<T>
      * push Creates a new node for the stack.
      * @param t The new node to push.
      */
-    public void push(T t)
-    {
+    public void push(T t) {
         Node<T> node = new Node<>(t);
         node.setNext(stackTop);
         stackTop = node;
@@ -63,14 +55,10 @@ public class LinkedListStack<T> implements CenturyStackInterface<T>
      * peek Throws EmptyStackPop if the stack is empty, otherwise returns the data of the most recently pushed node.
      * @return The data held by the top node.
      */
-    public T peek()
-    {
-        if(isEmpty())
-        {
+    public T peek() {
+        if(isEmpty()) {
             return null;
-        }
-        else
-        {
+        } else {
             return stackTop.getData();
         }
     }
@@ -78,8 +66,7 @@ public class LinkedListStack<T> implements CenturyStackInterface<T>
     /**
      * clear Dereferences the pushed nodes.
      */
-    public void clear()
-    {
+    public void clear() {
         stackTop = null;
         nodeCount = 0;
     }
@@ -88,12 +75,10 @@ public class LinkedListStack<T> implements CenturyStackInterface<T>
      * toString Returns the data held by each node in the stack as a String.
      * @return The data held by each node in the stack.
      */
-    public String toString()
-    {
+    public String toString() {
         StringBuilder str = new StringBuilder("[ ");
         Node<T> parser = stackTop;
-        while(parser != null)
-        {
+        while(parser != null) {
             str.append(parser.getData()).append(" ");
             parser = parser.getNext();
         }
@@ -103,27 +88,19 @@ public class LinkedListStack<T> implements CenturyStackInterface<T>
     /**
      * swapStart Swaps the first and second elements in the stack iteratively.
      */
-    public void swapStart()
-    {
-        if(stackTop == null || nodeCount < 2)
-        {
+    public void swapStart() {
+        if(stackTop == null || nodeCount < 2) {
             return;
-        }
-        else if(nodeCount == 2)
-        {
+        } else if(nodeCount == 2) {
             Node<T> parser = stackTop;
             Node<T> swap = parser.getNext();
             swap.setNext(parser);
             parser.setNext(null);
             stackTop = swap;
-        }
-        else
-        {
+        } else {
             Node<T> parser = stackTop;
-            while(parser.getNext() != null)
-            {
-                if(parser.getNext().getNext().getNext() == null)
-                {
+            while(parser.getNext() != null) {
+                if(parser.getNext().getNext().getNext() == null) {
                     Node<T> swap1 = parser.getNext();
                     Node<T> swap2 = parser.getNext().getNext();
                     swap2.setNext(swap1);
@@ -140,8 +117,7 @@ public class LinkedListStack<T> implements CenturyStackInterface<T>
      * editContents Returns the contents of the stack to be used with the stack editing program.
      * @return The contents of the stack.
      */
-    public String editContents()
-    {
+    public String editContents() {
         return null;
     }
 }
